17501f87e48f6feddbaac1fa9e2cbccfd13a4dc2
ndaemy <yuyaebean@gmail.com>
2021-12-31 01:51:58 +0900

    🗃️ Create applicant 2021 data type

---
+++ src/api/applicant-2021/controllers/applicant-2021.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  applicant-2021 controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::applicant-2021.applicant-2021');
---
+++ src/api/applicant-2021/services/applicant-2021.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * applicant-2021 service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::applicant-2021.applicant-2021');
