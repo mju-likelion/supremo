bbaea38ce854b1fff1ec8005b9b71d9d796b36de
ndaemy <yuyaebean@gmail.com>
2022-01-01 02:43:15 +0900

    🔧 Change database to postgres

--- config/database.js
+++ config/database.js
@@ -1,13 +1,17 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: 'postgres',
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'supremo'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', 'abcd1234'),
-      ssl: env.bool('DATABASE_SSL', false),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+      schema: env('DATABASE_SCHEMA', 'public'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
     },
+    debug: false,
   },
 });
